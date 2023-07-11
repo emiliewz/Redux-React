6f35305750997745ee706233391a3b5a3529d019
emilie <loveskyship@gmail.com>
2023-07-10 21:14:20 -0700

    update post slice, include post time ago, post author, post title, post content, and reaction buttons

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+// anytime we create a reducer, we need to add it to the store
+import postsReducer from "../features/posts/postsSlice";
+import usersReducer from "../features/users/usersSlice";
 
 export const store = configureStore({
   reducer: {
-    
+    posts: postsReducer,
+    users: usersReducer,
   },
 });
---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,77 @@
+// we can use reduxjs/toolkit nanoid to generate a random id, we do not need import a uuid npm package
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
+
+const initialState = [
+  {
+    id: "1",
+    title: "Learning Redux Toolkit",
+    content: "I've heard good things.",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
+  },
+  {
+    id: "2",
+    title: "Slices...",
+    content: "The more I say slice, the more I want pizza.",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
+  },
+];
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            date: new Date().toISOString(),
+            userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
+          },
+        };
+      },
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+      if (existingPost) {
+        // this is handled by mr.js, which allow code like this, that would normally mutate the state but underneath the hood emmer is making sure we are not mutating the state
+        // we can only do inside the create slice
+        existingPost.reactions[reaction]++;
+      }
+    },
+  },
+});
+
+export const selectAllPosts = (state) => state.posts;
+
+export const { postAdded, reactionAdded } = postsSlice.actions;
+
+export default postsSlice.reducer;
