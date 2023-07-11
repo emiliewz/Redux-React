378c1f1b2dc9011cacdd1619ee155f9bbcccbc0b
emilie <loveskyship@gmail.com>
2023-07-10 19:07:55 -0700

    add conuter reducer

---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  count: 0,
+};
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload;
+    },
+  },
+});
+// export two functions
+export const { increment, decrement, reset, incrementByAmount } =
+  counterSlice.actions;
+
+// export slice reducer, both of them must be exported
+export default counterSlice.reducer;
